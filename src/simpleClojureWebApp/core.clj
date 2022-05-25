a6024273498142e4b5337c2609807483f483eb34
minalchindhe <91877182+minalchindhe@users.noreply.github.com>
2022-05-25 13:18:16 +0530

    Updated

--- src/simpleClojureWebApp/core.clj
+++ src/simpleClojureWebApp/core.clj
@@ -1,17 +1,6 @@
-(ns simpleClojureWebApp.core
-  (:gen-class :main true)
-  (:use compojure.core
-        ring.middleware.json
-        ring.util.response
-        ring.adapter.jetty)
-  (:require [compojure.route :as route]
-            [simpleClojureWebApp.view :as view]))
+(ns clojure-project.core)
 
-(defroutes my_routes
-  (GET "/" [] (view/index-page))
-  (GET "/rest" [] (response {:email "sven@malvik.de"}))
-  (route/resources "/"))
-
-(def app (wrap-json-response my_routes))
-
-(defn -main [& args] (run-jetty app {:port 9001}))
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
